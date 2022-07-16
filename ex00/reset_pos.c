de6b0048252da80560d9e25ebc313c054a789320
Nino Colliot <nino.colliot@orage.fr>
2022-07-16 14:37:39 +0200

    norminette ok reset_pos

--- ex00/reset_pos.c
+++ ex00/reset_pos.c
@@ -6,16 +6,16 @@
 /*   By: ncolliot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 13:21:53 by ncolliot          #+#    #+#             */
-/*   Updated: 2022/07/16 14:29:14 by ncolliot         ###   ########.fr       */
+/*   Updated: 2022/07/16 14:36:45 by ncolliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	reset_pos(char *m[], int dim)
 {
-	int i;
-	int j;
-	
-	i  = 0;
+	int	i;
+	int	j;
+
+	i = 0;
 	while (i < dim)
 	{
 		j = 0;
@@ -27,4 +27,3 @@ void	reset_pos(char *m[], int dim)
 		i++;
 	}
 }
-
