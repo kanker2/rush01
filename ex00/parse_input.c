3a17b67e4195d53afaf5832144287d98400a5662
Luis Montes Guzman <lmontes-@c2r16s4.42madrid.com>
2022-07-16 20:41:22 +0200

    parse_input para procesar entrada y devolver matriz de sólo dígitos

---
+++ ex00/parse_input.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_input.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmontes- <lmontes-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 19:14:51 by lmontes-          #+#    #+#             */
+/*   Updated: 2022/07/16 20:40:39 by lmontes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+int	parse_input(char *argv, int *input_size)
+{
+	int	i;
+	int valid;
+	int n;
+	char *parsed_input;
+
+	i = 0;
+	n = 0;
+	valid = 1;
+	while (valid == 1  && argv[i] != '\0') //recorro el array inicial del parámetro de usuario
+	{
+		if(i%2==0)
+		{
+			valid = ('1' <= argv[i] && argv[i] <= 9); // comprueba que las posiciones pares son números
+			n++; //aumenta el contador de números
+		}
+		else
+			valid = (argv[i] = ' '); // comprueba posiciones  impares sean espacios
+		i++;
+	}
+	if (!valid)
+	   return (0);	
+	*input_size = n;
+    parsed_input = malloc(n);
+	i = 0; //reiniciamos contadores
+	n = 0;
+	while (argv[i] != '\0') 
+	{
+		parsed_input[n] = argv[i]; //escribimos los números, en posiciones pares, en el nuevo array, que devolvemos
+		n++;
+		i = i + 2;
+	}
+	return (*parsed_input); //devolvemos el puntero al nuevo array
+}
+/*
+int    main(int argc, char *argv[])
+{
+	int size;
+
+    size = 0;
+	parse_input(*argv, &size);
+}
+*/
