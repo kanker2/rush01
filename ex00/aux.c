1d68a61b1fdaf40f3ecda3ab59a352b81f02f2ea
kanaker2 <karce@ucm.es>
2022-07-16 18:45:23 +0200

    guay

--- ex00/aux.c
+++ ex00/aux.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aux.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: karce-ve <karce-ve@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 17:54:54 by karce-ve          #+#    #+#             */
+/*   Updated: 2022/07/16 18:44:25 by karce-ve         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int comprobar(char **m, int i, int j, int dim)
+int	check_row_col(char **m, int i, int j, int dim)
 {
-	int row;
-	int col;
-	printf("Comprobando:\nrow - %d ; col : %d --> %c\n", i, j, m[i][j]);
+	int	row;
+	int	col;
+
 	row = 0;
 	while (row < dim && (row == i || m[row][j] != m[i][j]))
 		row++;
@@ -15,30 +27,41 @@ int comprobar(char **m, int i, int j, int dim)
 	return (col == dim && row == dim);
 }
 
+int	user_input_check(char **m, char *user_input, int dim)
+{
+	int	t;
+	int	d;
+	int	l;
+	int	r;
+
+	t = top_check(m, user_input, dim);
+	d = down_check(m, user_input, dim);
+	l = left_check(m, user_input, dim);
+	r = right_check(m, user_input, dim);
+	return (t && d && l && r);
+}
+
 int	aux(char **matrix, int i, int j, int dim)
 {
 	int	char_to_try;
-	int have_solution;
-	int row_sum;
-	
-	if(j == dim - 1)
+	int	have_solution;
+	int	row_sum;
+
+	if (j == dim - 1)
 		row_sum = 1;
 	else
 		row_sum = 0;
-	if(i == dim)
-		return (1);
-	else if(matrix[i][j] != '0')
-		return aux(matrix, i + row_sum, (j + 1) % dim, dim);
+	if (i == dim)
+		return (user_input_check(matrix, , dim));
+	else if (matrix[i][j] != '0')
+		return (aux(matrix, i + row_sum, (j + 1) % dim, dim));
 	have_solution = 0;
   	char_to_try = 1;
-	while(!have_solution && char_to_try <= dim)
+	while (!have_solution && char_to_try <= dim)
 	{
 		matrix[i][j] = '0' + char_to_try;
-		if(comprobar(matrix, i, j, dim))
-		{
-			printf("----DECIDIDO:-----------------------------\nrow - %d ; col : %d --> %c\n^^^^^^^^^^^^^^^^^^^^^^\n", i, j, matrix[i][j]);
+		if (check_row_col(matrix, i, j, dim))
 			have_solution = aux(matrix, i + row_sum, (j + 1) % dim, dim);
-		}
 		else
 			matrix[i][j] = '0';
 		char_to_try++;
@@ -46,40 +69,40 @@ int	aux(char **matrix, int i, int j, int dim)
 	return (have_solution);
 }
 
-void mostrar(char **m, int dim)
-{
-	for(int i = 0; i < dim; i++){
-		for(int j = 0; j < dim; j++)
-			printf("%c ", m[i][j]);
-		printf("\n");
-	}
-}
-
-int main()
-{
-	int size =8;
-	char **m = malloc(sizeof(char*) * size);
-	int i = 0;
-	while (i < size)
-	{
-		m[i] = malloc(size);
-		i++;
-	}
-
-	int j;
-	i = 0;
-
-	while(i < size)
-	{
-		j = 0;
-		while(j < size)
-		{
-			m[i][j] = '0';
-			j++;
-		}
-		i++;
-	}
-	mostrar(m, size);
-	aux(m, 0, 0, size);
-	mostrar(m, size);
-}
+//void mostrar(char **m, int dim)
+//{
+//	for(int i = 0; i < dim; i++){
+//		for(int j = 0; j < dim; j++)
+//			printf("%c ", m[i][j]);
+//		printf("\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int size =8;
+//	char **m = malloc(sizeof(char*) * size);
+//	int i = 0;
+//	while (i < size)
+//	{
+//		m[i] = malloc(size);
+//		i++;
+//	}
+//
+//	int j;
+//	i = 0;
+//
+//	while(i < size)
+//	{
+//		j = 0;
+//		while(j < size)
+//		{
+//			m[i][j] = '0';
+//			j++;
+//		}
+//		i++;
+//	}
+//	mostrar(m, size);
+//	aux(m, 0, 0, size);
+//	mostrar(m, size);
+//}
---
+++ ex00/check_blocks.c
@@ -0,0 +1,132 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_blocks.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: karce-ve <karce-ve@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 18:41:03 by karce-ve          #+#    #+#             */
+/*   Updated: 2022/07/16 18:41:05 by karce-ve         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	check_blocks_seen(int n_to_see, int nums[], int size)
+{
+	int	n_blocks_seen;
+	int	i;
+	int	max;
+
+	max = 0;
+	n_blocks_seen = 0;
+	i = 0;
+	while (i < size)
+	{
+		if (max < nums[i])
+		{
+			n_blocks_seen++;
+			max = nums[i];
+		}
+		i++;
+	}
+	return (n_blocks_seen == n_to_see);
+}
+
+int	top_check(char **m, char *user_input, int dim)
+{
+	int	i;
+	int	j;
+	int	*n;
+	int	user_n;
+	int	valid;
+
+	valid = 1;
+	n = malloc(4 * dim);
+	i = 0;
+	while (valid && i < dim)
+	{
+		user_n = user_input[i] - '0';
+		j = 0;
+		while (j < dim)
+		{
+			n[j] = m[j][i];
+			j++;
+		}
+		valid = check_blocks_seen(user_n, n, dim);
+		i++;
+	}
+}
+
+int	down_check(char **m, char *user_input, int dim)
+{
+	int	i;
+	int	j;
+	int	*n;
+	int	user_n;
+	int	valid;
+
+	valid = 1;
+	n = malloc(4 * dim);
+	i = dim;
+	while (valid && i < 2 * dim)
+	{
+		user_n = user_input[i] - '0';
+		j = 0;
+		while (j < dim)
+		{
+			n[j] = m[dim - 1 - j][i];
+			j++;
+		}
+		valid = check_blocks_seen(user_n, n, dim);
+		i++;
+	}
+}
+
+int	left_check(char **m, char *user_input, int dim)
+{
+	int	i;
+	int	j;
+	int	*n;
+	int	user_n;
+	int	valid;
+
+	valid = 1;
+	n = malloc(4 * dim);
+	i = 2 * dim;
+	while (valid && i < 3 * dim)
+	{
+		user_n = user_input[i] - '0';
+		j = 0;
+		while (j < dim)
+		{
+			n[j] = m[i][j];
+			j++;
+		}
+		valid = check_blocks_seen(user_n, n, dim);
+		i++;
+	}
+}
+
+int	right_check(char **m, char *user_input, int dim)
+{
+	int	i;
+	int	j;
+	int	*n;
+	int	user_n;
+	int	valid;
+
+	valid = 1;
+	n = malloc(4 * dim);
+	i = 3 * dim;
+	while (valid && i < 4 * dim)
+	{
+		user_n = user_input[i] - '0';
+		j = 0;
+		while (j < dim)
+		{
+			n[j] = m[i][dim - 1 - j];
+			j++;
+		}
+		valid = check_blocks_seen(user_n, n, dim);
+		i++;
+	}
+}
