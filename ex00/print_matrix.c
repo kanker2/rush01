3a5843c9d2583f13d1c35c690c5159441cb50384
Luis Montes Guzman <lmontes-@c2r16s4.42madrid.com>
2022-07-16 17:33:42 +0200

    print_matrix.c == función para imprimir resultado

--- ex00/print_matrix.c
+++ ex00/print_matrix.c
@@ -6,7 +6,7 @@
 /*   By: lmontes- <lmontes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 13:52:16 by lmontes-          #+#    #+#             */
-/*   Updated: 2022/07/16 14:33:02 by lmontes-         ###   ########.fr       */
+/*   Updated: 2022/07/16 17:29:18 by lmontes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,13 @@ void	print_matrix(char **matrix, int dim)
 	int	j;
 
 	i = 0;
-	while (i < dim -1)
+	while (i < dim)
 	{
 		j = 0;
-		while (j < dim -1)
+		while (j < dim)
 		{
 			write(1, &(matrix[i][j]), 1);
+			write(1, " ", 1);
 			j++;
 		}
 		write(1, "\n", 1);
